ee040ca5580ac729b2e1cb5974a1325aae9459ad
Muhammed Murat Göktaş <43945614+mmgoktas38@users.noreply.github.com>
2019-05-24 01:53:26 +0300

    Add files via upload

---
+++ frmBill.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lokanta
+{
+    public partial class frmBill : Form
+    {
+        public frmBill()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGeriDon_Click(object sender, EventArgs e)
+        {
+            frmSiparis frm = new frmSiparis();
+            this.Close();
+            frm.Show();
+        }
+
+        private void btnCikis_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Çıkmak İstediğinizde Emin Misiniz?", "Uyarı !!!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                Application.Exit(); 
+            }
+        }
+
+
+        cSiparis cs = new cSiparis();
+        private void frmBill_Load(object sender, EventArgs e)
+        {
+            if (cGenel._ServisTurNo == 1)
+            {
+                lblAdisyonId.Text = cGenel._AdisyonId ;
+                txtIndirimTutari.TextChanged += new EventHandler(txtIndirimTutari_TextChanged);
+                cs.getByOrder(lvUrunler, Convert.ToInt32(lblAdisyonId.Text));
+                if (lvUrunler.Items.Count > 0)
+                {
+                    decimal toplam = 0;
+                    for (int i = 0; i < lvUrunler.Items.Count; i++)
+                    {
+                        toplam += Convert.ToDecimal(lvUrunler.Items[i].SubItems[3].Text);
+                    }
+
+                    lblToplamTutar.Text = string.Format("{0:0.000}", toplam);
+                    lblOdenecek.Text = string.Format("{0:0.000}", toplam);
+                    decimal kdv = Convert.ToDecimal(lblOdenecek.Text) * 18 / 100;
+                    lblKdv.Text = string.Format("{0:0.000}", kdv);
+                }
+
+                gbIndirim.Visible = true;
+                txtIndirimTutari.Clear();
+
+            }
+            else if (cGenel._ServisTurNo == 2)
+            {
+                lblAdisyonId.Text = cGenel._AdisyonId;
+                txtIndirimTutari.TextChanged += new EventHandler(txtIndirimTutari_TextChanged);
+                cs.getByOrder(lvUrunler, Convert.ToInt32(lblAdisyonId.Text));
+
+
+
+
+
+                if (lvUrunler.Items.Count > 0)
+                {
+                    decimal toplam = 0;
+                    for (int i = 0; i < lvUrunler.Items.Count; i++)
+                    {
+                        toplam += Convert.ToDecimal(lvUrunler.Items[i].SubItems[3].Text);
+                    }
+
+                    lblToplamTutar.Text = string.Format("{0:0.000}", toplam);
+                    lblOdenecek.Text = string.Format("{0:0.000}", toplam);
+                    decimal kdv = Convert.ToDecimal(lblOdenecek.Text) * 18 / 100;
+                    lblKdv.Text = string.Format("{0:0.000}", kdv);
+                }
+
+                gbIndirim.Visible = true;
+                txtIndirimTutari.Clear();
+            }
+        }
+
+        private void txtIndirimTutari_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Convert.ToDecimal(txtIndirimTutari.Text) < Convert.ToDecimal(lblToplamTutar.Text))
+                {
+                    try
+                    {
+                        lblIndirim.Text = string.Format("{0:0.000}", Convert.ToDecimal(txtIndirimTutari.Text));
+                    }
+                    catch (Exception)
+                    {
+                        lblIndirim.Text = string.Format("{0:0.000}", 0);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("İndirim Tutarı Toplam Tutardan Fazla Olamaz !!! ");
+                }
+            }
+            catch (Exception)
+            {
+                lblIndirim.Text = string.Format("{0:0.000}", 0);
+            }
+        }
+
+        private void chkIndirim_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkIndirim.Checked)
+            {
+                gbIndirim.Visible = true;
+            }
+            else
+            {
+                gbIndirim.Visible = false;
+            }
+        }
+
+        private void lblIndirim_TextChanged(object sender, EventArgs e)
+        {
+            if (Convert.ToDecimal(lblIndirim.Text) > 0)
+            {
+                decimal odenecek = 0;
+                lblOdenecek.Text = lblToplamTutar.Text;
+                odenecek = Convert.ToDecimal(lblOdenecek.Text) - Convert.ToDecimal(lblIndirim.Text);
+                lblOdenecek.Text = string.Format("{0:0.000}", odenecek);
+
+            }
+
+            decimal kdv = Convert.ToDecimal(lblOdenecek.Text) * 18/100;
+            lblKdv.Text = string.Format("{0:0.000}", kdv);
+        }
+
+        cMasalar masalar = new cMasalar();
+        cRezervasyon rezerve = new cRezervasyon(); //
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(cGenel._ServisTurNo==1)
+            {
+                int masaid = masalar.TableGetbyNumber(cGenel._ButtonName);
+                int musteriId = 0;
+                if(masalar.TableGetbyState(masaid,4)==true)
+                {
+                    musteriId = rezerve.getByClientIdFromRezervasyon(masaid);
+                }
+                else
+                {
+                    musteriId = 1;
+                }
+                int odemeTurId = 0;
+                if (rbNakit.Checked)
+                {
+                    odemeTurId = 1;
+                }
+                if (rbKrediKarti.Checked)
+                {
+                    odemeTurId = 2;
+                }
+                if (rbTicket.Checked)
+                {
+                    odemeTurId = 3;
+                }
+                cOdeme odeme = new cOdeme();
+
+               
+                //ADISYONID,ODEMETURID,MUSTERIID,ARATOPLAM,KDVTUTARI,TOPLAMTUTAR,INDIRIM
+
+                odeme.AdisyonID = Convert.ToInt32(lblAdisyonId.Text);
+                odeme.OdemeTurId = odemeTurId;
+                odeme.MusteriId = müşteriId;
+                odeme.AraToplam = Convert.ToDecimal(lblOdenecek.Text);
+                odeme.Kdvtutari = Convert.ToDecimal(lblKdv.Text);
+                odeme.GenelToplam = Convert.ToDecimal(lblToplamTutar.Text);
+                odeme.Indirim = Convert.ToDecimal(lblIndirim.Text);
+
+
+                bool result = odeme.billClose(odeme);
+                if (result)
+                {
+                    MessageBox.Show("Hesap Kapatılmıştır.");
+                    masalar.setChangeTableState(Convert.ToString(masaid), 1);
+
+                    cRezervasyon c = new cRezervasyon();
+                    c.rezervationclose(Convert.ToInt32(lblAdisyonId.Text));
+
+                    cAdisyon a = new cAdisyon();
+                    a.adisyonkapat(Convert.ToInt32(lblAdisyonId.Text), 0);
+
+                    this.Close();
+                    frmMasalar frm = new frmMasalar();
+                    frm.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Hesap Kapatılırken Bir Hata Oluştu. Lütfen Yetkililere bildirniz.");
+
+
+
+
+                }
+            }
+    }
+}
